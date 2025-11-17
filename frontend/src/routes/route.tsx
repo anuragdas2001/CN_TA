fbbab7504597e283b1498846d5a9c590f649ed9a
anuragdas2001 <anuragdas12921@gmail.com>
2025-11-17 20:46:59 +0530

    added routes for frontend

--- frontend/src/routes/route.tsx
+++ frontend/src/routes/route.tsx
@@ -1,6 +1,6 @@
 // src/routes/AppRoutes.tsx
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
@@ -8,13 +8,69 @@ import CustomerDashboard from "../pages/CustomerDashboard";
 import OfficerDashboard from "../pages/OfficerDashboard";
 import NotFoundPage from "../pages/NotFoundPage";
 
+import MainLayout from "../layouts/MainLayout";
+import { UserRole } from "@/types";
+import ProtectedRoute from "@/shared/ProtectedRoute";
+import ApplyLoan from "@/pages/ApplyLoan";
+import OfficerProfile from "@/pages/officerProfile";
+import CustomerProfile from "@/pages/customerProfile";
+
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Public routes WITHOUT navbar */}
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/customer/dashboard" element={<CustomerDashboard />} />
-      <Route path="/officer/dashboard" element={<OfficerDashboard />} />
+
+      {/* Protected routes WITH navbar using MainLayout */}
+      <Route element={<MainLayout />}>
+        {/* Customer Routes */}
+        <Route
+          path="/customer/dashboard/:id"
+          element={
+            <ProtectedRoute allowedRoles={[UserRole.CUSTOMER]}>
+              <CustomerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/customer/profile/:id"
+          element={
+            <ProtectedRoute allowedRoles={[UserRole.CUSTOMER]}>
+              <CustomerProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/customer/apply"
+          element={
+            <ProtectedRoute allowedRoles={[UserRole.CUSTOMER]}>
+              <ApplyLoan />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Officer Routes */}
+        <Route
+          path="/officer/dashboard/:id"
+          element={
+            <ProtectedRoute allowedRoles={[UserRole.OFFICER]}>
+              <OfficerDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/officer/profile/:id"
+          element={
+            <ProtectedRoute allowedRoles={[UserRole.OFFICER]}>
+              <OfficerProfile />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+
+      {/* Catch-all route */}
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
