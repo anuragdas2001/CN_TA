8081b167b24bb95c1ef7abc36ba95ce28597a50b
anuragdas2001 <anuragdas12921@gmail.com>
2025-11-17 20:50:40 +0530

    added app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,32 @@
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import AppRoutes from "./routes/route";
-
-
+import { AuthProvider } from "./context/AuthContext";
 import "./App.css";
 
 function App() {
- 
-
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <AuthProvider>
+        {/* All routes with their layouts */}
         <AppRoutes />
-      </BrowserRouter>
-    </>
+        
+        {/* Global Toast Notifications */}
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
